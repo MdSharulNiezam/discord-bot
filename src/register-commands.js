8c1d76e8123ae989f9c453926f3d0e73f9cd7e4c
MdSharulNiezam <sharulniezambusiness@gmail.com>
2023-09-24 22:01:31 +0800

    Adding Registering command

---
+++ src/register-commands.js
@@ -0,0 +1,24 @@
+const { token, GUILD_ID, CLIENT_ID } = require('./config.json');
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+    {
+        name: 'hey',
+        description: 'Replies with hey!',
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+        console.log('Registering slash commands...')
+        await rest.put(
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+            { body: commands }
+        );
+        console.log('Slash commands were registered succesfully!')
+    } catch (error) {
+        console.log(`there was an error: ${error}`);
+    }
+})();
