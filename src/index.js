f36ce91fa6a1d4b36041201352a7dd5d268bb6e3
MdSharulNiezam <sharulniezambusiness@gmail.com>
2023-09-24 22:01:50 +0800

    Slash command problem.

--- src/index.js
+++ src/index.js
@@ -14,29 +14,37 @@ client.on('ready', (c) => {
     console.log(`${c.user.tag} is online`);
 });
 
-client.on('messageCreate', (message) => {
-    console.log(message.content);
-    console.log(message);
-    if (message.author.bot) {
-        return;
-    };
+// client.on('messageCreate', (message) => {
+//     console.log(message.content);
+//     console.log(message);
+//     if (message.author.bot) {
+//         return;
+//     };
 
-    if (message.content === 'UwU') {
-        message.reply('UwU')
-    };
+//     if (message.content === 'UwU') {
+//         message.reply('UwU')
+//     };
 
-    if (message.content === 'Hello') {
-        message.reply('Yes master, I am here');
-    };
-    if (message.content === 'Who are you to me') {
-        message.reply('I am your slave, master')
-    };
-})
+//     if (message.content === 'Hello') {
+//         message.reply('Yes master, I am here');
+//     };
+//     if (message.content === 'Who are you to me') {
+//         message.reply('I am your slave, master')
+//     };
+// })
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+client.on('interactionCreate', (interaction) => {
+    if (interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'hey') {
+        interaction.reply('hey');
     }
 });
+
+// client.on('message', msg => {
+//     if (msg.content === 'ping') {
+//         msg.reply('pong');
+//     }
+// });
 
 client.login(token);
