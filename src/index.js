326d6452e68a120d443bc198fd4f91ad0bdd5168
MdSharulNiezam <sharulniezambusiness@gmail.com>
2023-09-24 19:50:13 +0800

    create config structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+const Discord = req('discord.js');
 const {Client, IntentsBitField } = req('discord.js');
 
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
-        
+        IntentsBitField.Flags.GuildsMembers,
+        IntentsBitField.Flags.GuildsMessages,
+        IntentsBitField.Flags.MessageContent,
     ]
-})
+});
+
+const config = req('./config.json')
+
+client.on('ready', () => {
+    console.log('I am ready');
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
+
+client.login(config.token);
